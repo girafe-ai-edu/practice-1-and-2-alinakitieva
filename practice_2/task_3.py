9de2deabd0d872ffe70087f5b5772d66e39355cd
Alina <96070394+alinakitieva@users.noreply.github.com>
2024-09-15 21:50:45 +0200

    Solve tasks

--- practice_2/task_3.py
+++ practice_2/task_3.py
@@ -3,9 +3,16 @@
 Write a program to calculate the body mass index (BMI) of a person. The user must enter their height and weight, after which you use one of the formulas below to determine the index.
 BMI = weight/height**2 
 """
-weight = input()
-height = input()
+weight = input("Please enter your weight in kilograms: ")
+height = input("Please enter your height in meters: ")
 
 
 #code here
+weight = float(weight)
+height = float(height)
+
+def bmi(weight, height):
+    return weight / height**2
+
+print("Your BMI is: ", bmi(weight, height))
 
--- practice_2/task_4.py
+++ practice_2/task_4.py
@@ -4,3 +4,10 @@ Develop a program that asks the user for an integer 4-digit number and calculate
 3 + 1 + 4 + 1 = 9
 
 """
+number = input("Please enter a 4-digit number: ")
+def sum_digits(number):
+    if len(number) != 4:
+        print("Please enter a 4-digit number")
+    print("The sum of the digits in the number is:", sum([int(digit) for digit in number]))
+
+sum_digits(number)
